5c2ee5f7992d3aa133ee7e823824f9a72b9920e0
R Arjun Krishna <76245572+Arjun-kris@users.noreply.github.com>
2026-06-30 00:46:33 +0530

    Update Vite config to include base path
    Added base path configuration for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     modules: {
+//       localsConvention: "camelCase",
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
-});
+  base: '/portfolio-react/', // 👈 ADD THIS LINE
+})
